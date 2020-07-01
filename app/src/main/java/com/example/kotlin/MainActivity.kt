00b4b76a12f28451d43c5fde9e8b86938cc88c46
박호준 <hankil417@naver.com>
2020-07-01 21:33:00 +0900

    when 추가 활용 연습 -in 법위 설정 / 콤마로 여러설정

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -22,13 +22,13 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("메인화면", "터치화면 눌림")
 
-            Log.e("메인화면", "에러 로그 찍어보기")
+            Log.e("메인화면", "에러 로그 찍어보기")}
 
             secondBtn.setOnClickListener {
                 Toast.makeText(this, "둘째 버튼 눌림", Toast.LENGTH_SHORT).show()
                 Log.d("메인화면", "둘째 버튼 눌림")
 
-            }
+
 
         }
         // 토스트띄우기 버튼이 눌리면
@@ -37,15 +37,40 @@ class MainActivity : AppCompatActivity() {
             //대입값이 String이면 변수 타입도 자동으로 String으로 결정됨.
             val inputContent = inputcontentEdt.text.toString()
 
+            Toast.makeText(this, inputContent, Toast.LENGTH_SHORT).show()
+
+
+
+
+//            상수(val) 변수(var) 의 차이
 //            val input : String
 //            input = "테스트"
 //            input = "테스트2"
-
+//           var
 
 
 
             //10은 정수. 정수를 num1에 대입.=> num1은 Int로 결정
             // (예시)val num1 = 10
+        }
+
+        ageBtn.setOnClickListener {
+
+            //입력한 나이를 Int로 저장하자.
+            val inputAge = ageEdt.text.toString().toInt()
+
+            //나이가 60이상이면 "어르신 입니다." 토스트
+            if(inputAge >= 60) {
+                Toast.makeText(this, "어르신 입니다.", Toast.LENGTH_SHORT).show()
+            }
+
+            //입력된 나이값ㅇ[ 따라 다른 처리 when
+            when (inputAge){
+                33 -> {Toast.makeText(this,"나랑 동갑이다", Toast.LENGTH_SHORT).show()}
+                20-> {Toast.makeText(this,"스무살이다.", Toast.LENGTH_SHORT).show()}
+//                in 10..19 -> {Toast.makeText(this,"10대다.", Toast.LENGTH_SHORT).show()}
+                else -> {Toast.makeText(this, "아무해당 없는 나이", Toast.LENGTH_SHORT).show()}
+            }
         }
 
     }
